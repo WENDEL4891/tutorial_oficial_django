d794eb49c577a0d2bdf6a8b1f02b867b8ed26e7a
wendel4891 <wendel4891@gmail.com>
2022-04-26 11:16:47 -0300

    criado app polls; criada rota polls e view index; e add requirements.txt.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9xo67x3kes+wtjt%a8ls%u*8*f2)p63bo^-m4&%51*9)-lkhgg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
