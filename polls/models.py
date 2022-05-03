3c956ac6162888eb2e687abb33b2a676a9625215
wendel4891 <wendel4891@gmail.com>
2022-05-03 11:01:18 -0300

    add método __str__ para os models; | add um método personalizado ao model Question.

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,6 @@
+import datetime
+from django.utils import timezone
+
 from django.db import models
 
 # Create your models here.
@@ -6,9 +9,17 @@ class Question(models.Model):
     question_text = models.TextField(max_length=200)
     pub_date = models.DateField('date published')
 
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now().date() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
+
+    def __str__(self):
+        return self.choice_text
